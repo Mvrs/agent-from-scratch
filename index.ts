da9787b41db3e6cdbd4b93cec2a3304e04cbeb0b
Mvrs <johnsonmarlon18@gmail.com>
2024-11-28 13:20:13 -0800

    completed agent-from-scratch workshop

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
-import { runLLM } from './src/llm'
-import { addMessages, getMessages } from './src/memory'
+import { runAgent } from './src/agent'
+import { tools } from './src/tools'
 
 const userMessage = process.argv.slice(2).join(' ')
 
@@ -9,13 +9,4 @@ if (!userMessage) {
   process.exit(1)
 }
 
-await addMessages([{ role: 'user', content: userMessage }])
-const messages = await getMessages()
-
-const response = await runLLM({
-  messages,
-})
-
-await addMessages([{ role: 'assistant', content: response }])
-
-console.log(response)
+await runAgent({ userMessage, tools })
--- src/llm.ts
+++ src/llm.ts
@@ -1,12 +1,28 @@
 import type { AIMessage } from '../types'
 import { openai } from './ai'
+import { zodFunction } from 'openai/helpers/zod'
+import { systemPrompt } from './systemPrompt'
 
-export const runLLM = async ({ messages }: { messages: AIMessage[] }) => {
+export const runLLM = async ({
+  messages,
+  tools,
+}: {
+  messages: AIMessage[]
+  tools: any[]
+}) => {
+  const formmatedTools = tools.map(zodFunction)
+
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     temperature: 0.1,
-    messages,
+    messages: [
+      { role: 'system', content: systemPrompt },
+      ...messages,
+    ],
+    tools: formmatedTools,
+    tool_choice: 'auto',
+    parallel_tool_calls: false,
   })
 
-  return response.choices[0].message.content
+  return response.choices[0].message
 }
